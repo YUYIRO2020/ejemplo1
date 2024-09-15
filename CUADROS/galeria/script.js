3cf19736435d843a3b1cdbdfec6a2fb35380ba61
YUYIRO2020 <143560658+YUYIRO2020@users.noreply.github.com>
2024-09-15 17:04:26 -0500

    Add files via upload

---
+++ CUADROS/galeria/script.js
@@ -0,0 +1,10 @@
+const Galeria_seccion = document.querySelectorAll(".cont-int");
+
+Galeria_seccion.forEach(item => {
+    
+        const Detalles = item.querySelector(".mostrar");
+        detalles.style.display = "block";
+
+    })
+
+
